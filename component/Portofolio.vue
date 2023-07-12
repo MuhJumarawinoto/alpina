97a6090247212110f1ab8bf3a6f752c8bfc087a1
MuhJumarawinoto <muhamadjumarawinoto.77@gmail.com>
2023-07-12 22:18:22 +0700

    perbaiki testimoni dan katalog

--- component/Portofolio.vue
+++ component/Portofolio.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- ======= Portfolio Section ======= -->
-    <div id="portfolio" class="portfolio-area area-padding fix">
+    <div id="portfolio" class="portfolio-area area-padding fix" style=" background-color: rgb(219, 226, 221)">
         <div class="container">
           <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -14,9 +14,10 @@
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <ul id="portfolio-flters">
                 <li data-filter="*" class="filter-active">All</li>
-                <li data-filter=".filter-app">App</li>
-                <li data-filter=".filter-card">Card</li>
-                <li data-filter=".filter-web">Web</li>
+                <li data-filter=".filter-acc">Accesoris</li>
+                <li data-filter=".filter-celana">Pants</li>
+                <li data-filter=".filter-tas">Bag</li>
+                <li data-filter=".filter-topi">Hat</li>
               </ul>
             </div>
           </div>
@@ -24,15 +25,15 @@
           <div class="row awesome-project-content portfolio-container">
     
             <!-- portfolio-item start -->
-            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-app portfolio-item">
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-acc portfolio-item">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="/img/portfolio/1.jpg" alt="" /></a>
+                  <a href="#"><img src="/item/aksesoris/1.jpg" alt="" /></a>
                   <div class="add-actions text-center">
                     <div class="project-dec">
                       <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/1.jpg">
-                        <h4>Business City</h4>
-                        <span>Web Development</span>
+                        <h4>Matras GNT006</h4>
+                        <h3>Rp 85.000</h3>
                       </a>
                     </div>
                   </div>
@@ -42,15 +43,15 @@
             <!-- portfolio-item end -->
     
             <!-- portfolio-item start -->
-            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-web">
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-acc">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="/img/portfolio/2.jpg" alt="" /></a>
+                  <a href="#"><img src="/item/aksesoris/2.jpg" alt="" /></a>
                   <div class="add-actions text-center">
                     <div class="project-dec">
                       <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/2.jpg">
-                        <h4>Blue Sea</h4>
-                        <span>Photosho</span>
+                        <h4>Jas Hujan Setelan Dewasa</h4>
+                        <h3>Rp 335.000</h3>
                       </a>
                     </div>
                   </div>
@@ -60,15 +61,15 @@
             <!-- portfolio-item end -->
     
             <!-- portfolio-item start -->
-            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-card">
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-acc">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="/img/portfolio/3.jpg" alt="" /></a>
+                  <a href="#"><img src="/item/aksesoris/3.jpg" alt="" /></a>
                   <div class="add-actions text-center">
                     <div class="project-dec">
                       <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/3.jpg">
-                        <h4>Beautiful Nature</h4>
-                        <span>Web Design</span>
+                        <h4>Jas Hujan Ponco Kancing Samping Bawah</h4>
+                        <h3>Rp 185.000</h3>
                       </a>
                     </div>
                   </div>
@@ -78,15 +79,15 @@
             <!-- portfolio-item end -->
     
             <!-- portfolio-item start -->
-            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-web">
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-acc">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="/img/portfolio/4.jpg" alt="" /></a>
+                  <a href="#"><img src="/item/aksesoris/4.jpg" alt="" /></a>
                   <div class="add-actions text-center">
                     <div class="project-dec">
                       <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/4.jpg">
-                        <h4>Creative Team</h4>
-                        <span>Web design</span>
+                        <h4>Jas Hujan Ponco Seleting Pinggir</h4>
+                        <h3>Rp 215.000</h3>
                       </a>
                     </div>
                   </div>
@@ -96,15 +97,15 @@
             <!-- portfolio-item end -->
     
             <!-- portfolio-item start -->
-            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-app">
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-acc">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="/img/portfolio/5.jpg" alt="" /></a>
+                  <a href="#"><img src="/item/aksesoris/5.jpg" alt="" /></a>
                   <div class="add-actions text-center text-center">
                     <div class="project-dec">
                       <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/5.jpg">
-                        <h4>Beautiful Flower</h4>
-                        <span>Web Development</span>
+                        <h4>Gelang Alpina</h4>
+                        <h3>Rp 16.500</h3>
                       </a>
                     </div>
                   </div>
@@ -114,23 +115,152 @@
             <!-- portfolio-item end -->
     
             <!-- portfolio-item start -->
-            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-web">
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-celana">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="/img/portfolio/6.jpg" alt="" /></a>
+                  <a href="#"><img src="/item/celana/1.jpg" alt="" /></a>
                   <div class="add-actions text-center">
                     <div class="project-dec">
                       <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
-                        <h4>Night Hill</h4>
-                        <span>Photoshop</span>
+                        <h4>Celana Panjang Langsung Formil</h4>
+                        <h3>Rp 335.000</h3>
                       </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <!-- portfolio-item end -->
+            <!-- portfolio-item end ini  -->
+
+            <!-- portfolio-item start -->
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-celana">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/celana/2.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Celana Panjang Langsung</h4>
+                        <h3>Rp 335.500</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-celana">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/celana/3.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Celana Pendek Alpina</h4>
+                        <h3>Rp 277.500</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-tas">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/tas/1.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Daypack Alpina R241</h4>
+                        <h3>Rp 305.000</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
     
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-tas">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/tas/2.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Sling Bag Alpina T119</h4>
+                        <h3>Rp 125.000</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-tas">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/tas/3.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Carrier Alpina R 40</h4>
+                        <h3>Rp 450.000</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-topi">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/topi/1.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Topi Alpina Trucker Legend</h4>
+                        <h3>Rp 110.000</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-topi">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/topi/2.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Topi Alpina Reborn</h4>
+                        <h3>Rp 110.000</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 portfolio-item filter-topi">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="/item/topi/3.jpg" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="portfolio-lightbox" data-gallery="myGallery" href="img/portfolio/6.jpg">
+                        <h4>Topi Komando Alpina</h4>
+                        <h3>Rp 87.500</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div><!-- End Portfolio Section -->
--- component/Testimoni.vue
+++ component/Testimoni.vue
@@ -1,20 +1,25 @@
 <template>
     <!-- ======= Testimonials Section ======= -->
     <!-- ======= Testimonials Section ======= -->
-    <div id="testimonials" class="testimonials">
+    
+        <div class=" text-center pt-5 pb-2" style="background-color:rgb(47, 58, 41);">
+          <h2 style="font-family:'Jacques Francois', serif;color:white; font-size: 48px;">WHAT THEY SAY ABOUT US</h2>
+        </div>
+    
+
+    <div id="testimonials" class="testimonials pt-0">
       <div class="container">
     
         <div class="testimonials-slider swiper">
           <div class="swiper-wrapper">
-    
             <div class="swiper-slide">
               <div class="testimonial-item">
-                <img src="/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                <h3>Saul Goodman</h3>
-                <h4>Ceo &amp; Founder</h4>
+                <img src="/testi/engkos.jpg" class="testimonial-img" alt="">
+                <h3>engkos</h3>
+                <h4>Konsumen | Tokopedia</h4>
                 <p>
                   <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                    Legend yg kembali lagi, repeat order terus ah, tq
                   <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                 </p>
               </div>
@@ -22,12 +27,12 @@
     
             <div class="swiper-slide">
               <div class="testimonial-item">
-                <img src="/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                <h3>Sara Wilsson</h3>
-                <h4>Designer</h4>
+                <img src="/testi/dani.jpg" class="testimonial-img" alt="">
+                <h3>Dani</h3>
+                <h4>Konsumen | Tokopedia</h4>
                 <p>
                   <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                  barang sudah diterima, kualitas memuaskan...
                   <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                 </p>
               </div>
@@ -35,12 +40,12 @@
     
             <div class="swiper-slide">
               <div class="testimonial-item">
-                <img src="/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                <h3>Jena Karlis</h3>
-                <h4>Store Owner</h4>
+                <img src="/testi/yi.jpg" class="testimonial-img" alt="">
+                <h3>Y***i</h3>
+                <h4>Konsumen | Tokopedia</h4>
                 <p>
                   <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                  proses cepat, bahan lentur seperti daun talas
                   <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                 </p>
               </div>
@@ -48,12 +53,12 @@
     
             <div class="swiper-slide">
               <div class="testimonial-item">
-                <img src="/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                <h3>Matt Brandon</h3>
-                <h4>Freelancer</h4>
+                <img src="/testi/engkos.jpg" class="testimonial-img" alt="">
+                <h3>Sigit</h3>
+                <h4>Konsumen | Tokopedia</h4>
                 <p>
                   <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                  Ramah responnya barangnya juga sesuai deskripsi... keren laah keren keren Alpina idola dr dulu
                   <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                 </p>
               </div>
@@ -61,12 +66,12 @@
     
             <div class="swiper-slide">
               <div class="testimonial-item">
-                <img src="/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                <h3>John Larson</h3>
-                <h4>Entrepreneur</h4>
+                <img src="/testi/engkos.jpg" class="testimonial-img" alt="">
+                <h3>Rakhmat</h3>
+                <h4>Konsumen | Tokopedia</h4>
                 <p>
                   <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                  Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                  brg sesuai deskripsi, fast response & cepat sampai. dibonusin pula.... makin sukses kak
                   <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                 </p>
               </div>
@@ -78,5 +83,6 @@
     
       </div>
     </div><!-- End Testimonials Section -->
+
     <slot />
     </template>
